9872a4df2ddba06fb4a2f50671ff0232e47f48e0
Cibele Martins <Cibele.Martins@ibm.com>
2022-11-21 09:12:52 -0300

    🚧 feat: Alterou o button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,13 @@ import styles from './Button.module.css'
 // import { useContext } from 'react';
 // import { AuthContext } from '../../context/use-auth';
 
-const CustomButton = ({text, onClick, type})=> {
+const CustomButton = ({text, onClick, type, className})=> {
 
     // const ctx = useContext(AuthContext)
 
     // ctx.printConsole()
 
-    return <button onClick={onClick} type={type} className={styles.btn}>{text}</button>
+    return <button onClick={onClick} type={type} className={`${styles.btn} || ${className}`}>{text}</button>
 };
 
 
